460227ba2fbacf291747d3b0919ee3529d7968ea
Maxime <maxupcreation@gmail.com>
2020-10-09 14:31:22 +0200

    Reorganize

--- RPGTEST/Controller/ViewController.swift
+++ RPGTEST/Controller/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//MARK: - VARIABLE
+    @IBOutlet weak var situationLabel: UILabel!
+    
+    @IBOutlet var ChoiceCollectionButton: [UIButton]!
+    
+    
+    @IBOutlet var barInformationLabelCollection: [UILabel]!
 
+    //MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        }
+
+    
+    
 
 
+    
 }
 
---
+++ RPGTEST/Model/GameModel.swift
@@ -0,0 +1,9 @@
+//
+//  GameModel.swift
+//  RPGTEST
+//
+//  Created by Maxime on 09/10/2020.
+//  Copyright © 2020 Maxime. All rights reserved.
+//
+
+import Foundation
